09ad1c47dc067476d3432238bd374f1d81da182e
hyodoblog <yusei.hyodo@gmail.com>
2023-05-01 17:48:38 +0900

    ♻️

--- pages/api/linebot/usecase.ts
+++ pages/api/linebot/usecase.ts
@@ -1,5 +1,4 @@
 import { WebhookEvent } from '@line/bot-sdk'
-import { lineClient } from './client'
 import { issueZoomURL } from './issue-zoom-url'
 
 export const usecases = async (event: WebhookEvent) => {
