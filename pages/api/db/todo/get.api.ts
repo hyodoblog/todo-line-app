f3dc940bd9dd402db639f109924492647b1da7f9
hyodoblog <yusei.hyodo@gmail.com>
2023-05-03 09:25:33 +0900

    🚑 add todo api.

---
+++ pages/api/db/todo/get.api.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '~/libs/prisma'
+
+export default async function handler(_request: NextApiRequest, response: NextApiResponse) {
+  try {
+    const data = await prisma.post.findMany()
+
+    return response.status(200).json({ data })
+  } catch (error) {
+    return response.status(500).json({ error })
+  }
+}
