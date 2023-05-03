4ea24023e693b842cc52f5b42b0f6d06544d8203
hyodoblog <yusei.hyodo@gmail.com>
2023-05-03 20:45:32 +0900

    ♻️ zoom word list.

--- pages/api/linebot/usecases/index.ts
+++ pages/api/linebot/usecases/index.ts
@@ -1,8 +1,20 @@
 import { WebhookEvent } from '@line/bot-sdk'
 import { issueZoomURL } from './issue-zoom-url'
 
+const zoomWordList = ['Zoom', 'zoom', 'ZOOM', 'ズーム']
+
 export const usecases = async (event: WebhookEvent) => {
-  if (event.type === 'message' && event.message.type === 'text' && event.message.text === 'Zoom') {
-    await issueZoomURL(event)
+  // issue zoom url.
+  if (
+    event.type === 'message' &&
+    event.message.type === 'text' &&
+    zoomWordList.includes(event.message.text)
+  ) {
+    return await issueZoomURL(event)
+  }
+
+  // todo store.
+  if (event.type === 'message' && event.message.type === 'text') {
+    return await issueZoomURL(event)
   }
 }
