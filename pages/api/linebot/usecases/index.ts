0bd597fa14d10056e4535b20c3bc379faebb6822
hyodoblog <yusei.hyodo@gmail.com>
2023-05-03 21:10:30 +0900

    ✨

--- pages/api/linebot/usecases/index.ts
+++ pages/api/linebot/usecases/index.ts
@@ -1,6 +1,7 @@
 import { WebhookEvent } from '@line/bot-sdk'
 import { issueZoomURLUsecase } from './issue-zoom-url'
 import { addTaskUsecase } from './add-task'
+import { viewTasksUsecase } from './view-tasks.ts'
 
 const zoomWordList = ['Zoom', 'zoom', 'ZOOM', 'ズーム']
 
@@ -14,7 +15,12 @@ export const usecases = async (event: WebhookEvent) => {
     return await issueZoomURLUsecase(event)
   }
 
-  // todo store.
+  // view todo list.
+  if (event.type === 'message' && event.message.type === 'text' && event.source.type === 'user') {
+    return await viewTasksUsecase(event)
+  }
+
+  // add task.
   if (event.type === 'message' && event.message.type === 'text' && event.source.type === 'user') {
     return await addTaskUsecase(event, event.message, event.source)
   }
---
+++ pages/api/linebot/usecases/view-tasks.ts.ts
@@ -0,0 +1,57 @@
+import { MessageEvent, FlexMessage, FlexComponent } from '@line/bot-sdk'
+import { lineClient } from '../client'
+import { prisma } from '~/libs/prisma'
+import { Task } from '@prisma/client'
+
+const getMessages = (tasks: Task[]): FlexMessage => {
+  const taskFlexComponent: FlexComponent[] = tasks.map((task): FlexComponent => {
+    return {
+      type: 'box',
+      layout: 'baseline',
+      contents: [
+        {
+          type: 'text',
+          text: task.title,
+          weight: 'regular',
+          flex: 7,
+          margin: 'sm',
+          wrap: true
+        },
+        {
+          type: 'text',
+          text: '削除',
+          size: 'md',
+          color: '#FF2E2EFF',
+          flex: 2,
+          align: 'end',
+          action: {
+            type: 'postback',
+            label: '削除',
+            displayText: '削除',
+            data: `delete_task_${task.id}`
+          }
+        }
+      ]
+    }
+  })
+
+  return {
+    type: 'flex',
+    altText: 'タスク一覧',
+    contents: {
+      type: 'bubble',
+      body: {
+        type: 'box',
+        layout: 'vertical',
+        spacing: 'md',
+        contents: taskFlexComponent
+      }
+    }
+  }
+}
+
+export const viewTasksUsecase = async (event: MessageEvent) => {
+  const tasks = await prisma.task.findMany()
+
+  await lineClient.replyMessage(event.replyToken, getMessages(tasks))
+}
