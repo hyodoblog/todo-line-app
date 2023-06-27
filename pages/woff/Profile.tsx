fd29a822c5063d1a226c8a876a5c5437c3378560
hyodoblog <yusei.hyodo@gmail.com>
2023-06-27 23:59:38 +0900

    🐛

--- pages/woff/Profile.tsx
+++ pages/woff/Profile.tsx
@@ -1,14 +1,37 @@
-import { useContext } from 'react'
+import { Task } from '@prisma/client'
+import { useContext, useEffect, useState } from 'react'
+import { prismaClient } from '~/clients/prisma.client'
+import { MiniLoader } from '~/components/Loader/Mini'
 import { WoffContext } from '~/contexts/WoffContext'
 
 export const WoffProfile = () => {
   const { profile } = useContext(WoffContext)
+  const [tasks, setTasks] = useState<Task[] | null>(null)
+
+  useEffect(() => {
+    ;(async () => {
+      if (profile) {
+        const res = await fetch('api/db/tasks/get')
+        const data = await res.json()
+        setTasks(data as Task[])
+      }
+    })()
+  }, [profile])
 
   return (
     <div>
-      <div>ddd</div>
-      <div>{profile?.userId}</div>
-      <div>{profile?.displayName}</div>
+      {tasks === null ? (
+        <MiniLoader />
+      ) : (
+        <div>
+          {tasks.map((task) => (
+            <div key={task.id}>
+              <div>{task.title}</div>
+              <div>{task.description}</div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
